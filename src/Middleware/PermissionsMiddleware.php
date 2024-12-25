c591d7b04fdc0fb85c917e1146fc842ee9a9e7cd
eramitgupta <info.eramitgupta@gmail.com>
2024-12-25 15:51:25 +0530

    feat: add middleware permissions

---
+++ src/Middleware/PermissionsMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EragPermission\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PermissionsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$permissions): Response
+    {
+        if (! $request->user()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        if (! $request->user()->hasPermissions($permissions)) {
+            abort(403, 'You do not have the required permission.');
+        }
+
+        return $next($request);
+    }
+}
